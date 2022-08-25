a4d4b09f67a40aac5e3fa15d0c15435d822a6452
desa16-ibero <desarrollo16@ibero.mx>
2022-08-25 12:36:33 -0500

    changes in splash

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/custom_colors.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/icon_app.png',
+              alignment: Alignment.topCenter,
+              fit: BoxFit.scaleDown,
+              height: 128,
+            ),
+            const Text(
+              'Planta Física',
+              style: TextStyle(fontSize: 25),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+                backgroundColor: CustomColors.dartMainColor),
+          ],
+        ),
+      ),
+    );
   }
 }
